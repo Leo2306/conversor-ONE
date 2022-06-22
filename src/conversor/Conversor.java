4ee699158ee3fb90942115ea59f78e01a6ad2cde
Leonardo <leonardonarvaja232@gmail.com>
2022-06-21 23:59:41 -0300

    Actualizando proyecto

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+//  - Convertir de la moneda de tu país a Dólar
+//       - Convertir de la moneda de tu país  a Euros
+//       - Convertir de la moneda de tu país  a Libras Esterlinas
+//       - Convertir de la moneda de tu país  a Yen Japonés
+//       - Convertir de la moneda de tu país  a Won sul-coreano
+
+package conversor;
+
+import conversor.vista.frmPrincipal;
+
+/**
+ *
+ * @author leona
+ */
+public class Conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal abrir = new frmPrincipal();
+        abrir.setVisible(true);
+    }
+    
+}
